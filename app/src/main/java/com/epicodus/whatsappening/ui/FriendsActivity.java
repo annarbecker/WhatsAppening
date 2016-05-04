c429b5092c701b5d9cc8d0bf6084421bdf5d55ae
Anna Becker and Matt Rosanio <anna.r.becker@gmail.com>
2016-05-04 11:05:56 -0700

    Add logout functionality

--- app/src/main/java/com/epicodus/whatsappening/ui/FriendsActivity.java
+++ app/src/main/java/com/epicodus/whatsappening/ui/FriendsActivity.java
@@ -1,9 +1,13 @@
 package com.epicodus.whatsappening.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.epicodus.whatsappening.Constants;
 import com.epicodus.whatsappening.R;
@@ -19,6 +23,7 @@ public class FriendsActivity extends AppCompatActivity {
     private Query mQuery;
     private Firebase mFirebaseFriendsRef;
     private FirebaseFriendListAdapter mAdapter;
+    private Firebase mFirebaseRef;
 
     @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
 
@@ -29,6 +34,7 @@ public class FriendsActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         mFirebaseFriendsRef = new Firebase(Constants.FIREBASE_URL_FRIENDS);
+        mFirebaseRef = new Firebase(Constants.FIREBASE_URL);
 
         setUpFirebaseQuery();
         setUpRecyclerView();
@@ -43,5 +49,34 @@ public class FriendsActivity extends AppCompatActivity {
         mAdapter = new FirebaseFriendListAdapter(mQuery, Friend.class);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void logout() {
+        mFirebaseRef.unauth();
+        takeUserToLoginScreenOnUnAuth();
+    }
+
+    private void takeUserToLoginScreenOnUnAuth() {
+        Intent intent = new Intent(FriendsActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
